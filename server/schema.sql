e736cbbe0cb37f909685771f94853c36a26268b1
Ben Grinberg <bengrin25@gmail.com>
2015-12-18 18:48:44 -0500

    schema and db/index.html

--- server/schema.sql
+++ server/schema.sql
@@ -1,17 +1,30 @@
+DROP DATABASE IF EXISTS chat;
 CREATE DATABASE chat;
 
 USE chat;
 
 CREATE TABLE messages (
-  /* Describe your table here.*/
+
+  message_id INT NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (message_id),
+  text VARCHAR(140) NOT NULL,
+  roomname_id INT NOT NULL,
+  name_id INT NOT NULL
+
 );
 
-/* Create other tables and define schemas for them here! */
+CREATE TABLE users (
+
+  user_id INT NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (user_id),
+  name VARCHAR(40) NOT NULL
+);
+
+CREATE TABLE rooms (
+  room_id INT NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (room_id),
+  roomname VARCHAR(20) NOT NULL
+);
 
 
-
-
-/*  Execute this file from the command line by typing:
- *    mysql -u root < server/schema.sql
- *  to create the database and the tables.*/
 
